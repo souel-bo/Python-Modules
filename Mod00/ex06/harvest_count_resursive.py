b407fb5fc2b8283df4658e6831ea654db7d1cc05
Soufiane El Bourakkadi <souel-bo@c4r8p5.1337.ma>
2026-07-26 20:37:38 +0100

    ADD : Harvest recursion

--- Mod00/ex06/harvest_count_resursive.py
+++ Mod00/ex06/harvest_count_resursive.py
@@ -1,8 +1,13 @@
-def harvest_recursive(current_day, total_days):
-    days = input("Days untill harvest : ")
-    i = 1
-    if i > int(days):
+def recursion(i, days):
+    if (i > days):
+        print("Harvest Time!")
         return
     print("Day ", i)
-    harvest_recursive()
-    print("Harvest Time!")
+    recursion(i + 1, days)
+
+def harvest_recursive():
+    days = input("Days untill harvest : ")
+    i = 1
+    recursion(i, int(days))
+
+harvest_recursive()
