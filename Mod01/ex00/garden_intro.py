115519e723acba5f23887c99ad3e932d83c4a317
Soufiane El Bourakkadi <souel-bo@c3r1p4.1337.ma>
2026-07-31 16:49:38 +0100

    ADD : First Ex Of Mod01

---
+++ Mod01/ex00/garden_intro.py
@@ -0,0 +1,12 @@
+def main():
+    print("======== Welcome To My Garden ========")
+    name = "Rose"
+    height =  25
+    age = 30
+    print("Plant : ", name)
+    print("Height : ", height, "Cm")
+    print("Age : ", age, "Day")
+
+
+if __name__ == "__main__":
+    main()
